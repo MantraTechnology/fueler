28f200bdd79bfd98603e58188d811c382a6d8cb7
Adron Hall <adronhall@gmail.com>
2014-03-15 19:42:53 -0700

    Basic environmental settings using convict.js.

--- config.js
+++ config.js
@@ -8,22 +8,22 @@ var convict = require('convict');
 // Schema
 var conf = convict({
     env: {
-        doc: "The applicaton environment.",
+        doc: "The App Environment.",
         format: ["production", "development", "test"],
         default: "development",
         env: "NODE_ENV"
-    },
-    ip: {
-        doc: "The IP address to bind.",
-        format: "ipaddress",
-        default: "127.0.0.1",
-        env: "IP_ADDRESS"
     },
     port: {
         doc: "The port to bind.",
         format: "port",
         default: 3000,
         env: "PORT"
+    },
+    database: {
+        host: {
+            default: "someplace:cool",
+            env: "DB_HOST"
+        }
     }
 });
 
